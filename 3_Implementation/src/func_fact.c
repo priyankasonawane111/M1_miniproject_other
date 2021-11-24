5663f5c1242a370c92490ae018746ee0abe6ad59
priyankasonawane111 <94418525+priyankasonawane111@users.noreply.github.com>
2021-11-24 23:13:04 +0530

    Add files via upload

---
+++ 3_Implementation/src/func_fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "header.h"
+
+/**
+ * @brief factorial function which returns the desired result using answer variable
+ * 
+ * @param n 
+ * @param answer 
+ * @return error 
+ */
+
+error add(calc_arthmetic n,result* answer)
+{
+    answer->final_answer=n.num_1!;
+    return SUCCESS;
+}
